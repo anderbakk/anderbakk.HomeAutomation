1d61b6e92e4fe6b5fc0a490b26bfff22d420c157
anderbakk <thor@anderbakk.com>
2015-02-13 23:28:24 +0100

    Moved device controller to separate assembly. tested turning on light with by a GET action.

---
+++ anderbakk.HomeAutomation/anderbakk.HomeAutomation.Tellstick/Controllers/DevicesController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+using System.Web.Http.Results;
+using TelldusCoreWrapper;
+using TelldusCoreWrapper.Entities;
+using TelldusCoreWrapper.Service;
+
+namespace anderbakk.HomeAutomation.Tellstick.Controllers
+{
+    public class DevicesController : ApiController
+    {
+        private TelldusCoreService _telldusCoreService;
+
+        public DevicesController()
+        {
+            _telldusCoreService = new TelldusCoreService(new TelldusCoreLibraryWrapper());
+        }
+
+        [Route("Devices")]
+        public IEnumerable<ReceiverDevice> GetAll()
+        {
+            return _telldusCoreService.GetDevices();
+        }
+
+        [Route("Devices/{deviceId}")]
+        public IHttpActionResult Get(int deviceId)
+        {
+            
+                _telldusCoreService.TurnOn(deviceId);
+            return Ok();
+        }
+
+        [Route("Sensor")]
+        public IEnumerable<Sensor> GetAllSensors()
+        {
+            return _telldusCoreService.GetSensors();
+        }
+
+    }
+}
